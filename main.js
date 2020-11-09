6361ebc46fff0a0df79d2b2b8a07319858bfb792
Saurav Chamoli <sauravchamoli17@gmail.com>
2020-11-09 15:22:40 +0530

    Final Commit

--- main.js
+++ main.js
@@ -7,10 +7,8 @@ function handleCheck(e) {
     let inBetween = false;
     if (e.shiftKey && this.checked) {
         checkboxes.forEach(checkbox => {
-            console.log(checkbox);
             if (checkbox === this || checkbox === lastChecked) {
                 inBetween = !inBetween;
-                console.log('Starting to Check in Between!');
             }
             if (inBetween) {
                 checkbox.checked = true;
