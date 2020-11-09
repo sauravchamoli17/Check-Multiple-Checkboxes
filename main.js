5183aedfa3f50ff0fed211929c8b807bfe01a690
Saurav Chamoli <sauravchamoli17@gmail.com>
2020-11-09 14:44:36 +0530

    Final Commit!

---
+++ main.js
@@ -0,0 +1,23 @@
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let lastChecked;
+
+function handleCheck(e) {
+    // Check if the shift key is down
+    // And the input is checked
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log('Starting to Check in Between!');
+            }
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
